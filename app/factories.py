f55666ec20c5c7e89a27bddc0209252e65ae1ac7
anistratov1302-art <anistratov1302@gmail.com>
2025-11-27 12:05:27 +0300

    Update factories.py

--- app/factories.py
+++ app/factories.py
@@ -1,6 +1,9 @@
 import factory
 from faker import Faker
-from module_30_ci_linters.homework.app.models import Client, Parking
+from module_30_ci_linters.homework.app.models import (
+    Client,
+    Parking,
+)
 from module_30_ci_linters.homework.app.extensions import db
 
 fake = Faker()
@@ -15,9 +18,8 @@ class ClientFactory(factory.alchemy.SQLAlchemyModelFactory):
     name = factory.Faker('first_name')
     surname = factory.Faker('last_name')
     credit_card = factory.LazyFunction(
-    lambda: fake.credit_card_number() if fake.boolean() else None
-)
-
+        lambda: fake.credit_card_number() if fake.boolean() else None
+    )
     car_number = factory.Faker('bothify', text='???-####')
 
 
@@ -26,9 +28,10 @@ class ParkingFactory(factory.alchemy.SQLAlchemyModelFactory):
         model = Parking
         sqlalchemy_session = db.session
         sqlalchemy_session_persistence = 'commit'
-        
 
     address = factory.Faker('address')
     opened = factory.Faker('boolean')
     count_places = factory.Faker('random_int', min=5, max=100)
-    count_available_places = factory.LazyAttribute(lambda obj: obj.count_places)
+    count_available_places = factory.LazyAttribute(
+        lambda obj: obj.count_places
+    )
